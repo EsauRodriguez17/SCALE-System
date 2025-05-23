a403f8cfe2d85e3eeb5608c5bdef2da75eaa00b0
Esau Rodriguez <esau5271@gmail.com>
2025-05-23 00:46:17 -0600

    Modulo completo de gestion de productos

--- database/queries.py
+++ database/queries.py
@@ -15,3 +15,89 @@ LEFT JOIN variantes_producto vp ON p.id_producto = vp.id_producto
 GROUP BY p.id_producto
 ORDER BY p.id_producto ASC;
 """
+GET_PRODUCTOS_CON_VARIANTES = """
+SELECT 
+    p.id_producto,
+    p.nombre,
+    p.descripcion,
+    p.precio_base,
+    pr.id_proveedor,
+    pr.nombre AS proveedor_nombre,
+    c.id_categoria,
+    c.nombre AS categoria_nombre,
+    vp.id_variante     AS id_variante,
+    vp.talla,
+    vp.color,
+    vp.stock           AS stock_variante,
+    vp.sku
+FROM productos p
+LEFT JOIN proveedores pr ON p.id_proveedor = pr.id_proveedor
+LEFT JOIN categorias c ON p.id_categoria = c.id_categoria
+LEFT JOIN variantes_producto vp ON p.id_producto = vp.id_producto
+ORDER BY p.id_producto, vp.id_variante;
+"""
+
+GET_PRODUCTO_BY_ID="SELECT * FROM productos WHERE id_producto = ?"
+
+INSERT_PRODUCTO = """
+INSERT INTO productos (nombre, descripcion, precio_base, id_proveedor, id_categoria, fecha_creacion)
+VALUES (?, ?, ?, ?, ?, ?);
+"""
+UPDATE_PRODUCTO = """
+UPDATE productos 
+SET nombre = ?, descripcion = ?, precio_base = ?, id_proveedor = ?, id_categoria = ?
+WHERE id_producto = ?;
+"""
+DELETE_PRODUCTO = "DELETE FROM productos WHERE id_producto = ?"
+
+# Variantes de Producto
+
+GET_VARIANTES_BY_PRODUCTO = """
+SELECT id_variante, id_producto, talla, color, stock, sku
+FROM variantes_producto
+WHERE id_producto = ?;
+"""
+GET_VARIANTE_BY_ID = """
+SELECT id_variante, id_producto, talla, color, stock, sku  
+FROM variantes_producto
+WHERE id_variante = ?;
+"""
+
+INSERT_VARIENTE = """
+INSERT INTO variantes_producto (id_producto, talla, color, stock, sku)
+VALUES (?, ?, ?, ?, ?)""" 
+
+UPDATE_VARIANTE = """
+UPDATE variantes_producto
+SET talla = ?, color = ?, stock = ?
+WHERE id_variante = ?;
+"""
+
+DELETE_VARIANTE = """
+DELETE FROM variantes_producto
+WHERE id_variante = ?;
+"""
+
+# Ofertas
+INSERT_OFERTA = """
+INSERT INTO ofertas (id_producto, porcentaje_descuento, fecha_inicio, fecha_fin) 
+VALUES (?, ?, ?, ?)
+"""
+
+UPDATE_OFERTA = """
+UPDATE ofertas 
+SET porcentaje_descuento = ?, fecha_inicio = ?, fecha_fin = ?
+WHERE id_oferta = ?;
+"""
+
+DELETE_OFERTA = "DELETE FROM ofertas WHERE id_oferta = ?"
+
+GET_OFERTA_BY_IDPRODUCTO="SELECT * FROM ofertas WHERE id_producto = ?"
+
+# Proveedores
+
+GET_ALL_PROVEEDORES = "SELECT * FROM proveedores ORDER BY id_proveedor ASC;"
+
+
+# Categorías
+GET_ALL_CATEGORIAS = "SELECT * FROM categorias ORDER BY id_categoria ASC;"
